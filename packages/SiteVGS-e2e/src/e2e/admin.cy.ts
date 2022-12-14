7564f5c94e085d7b2cefb145bf264e3ed24dc752
Gurvan B <gurvan.buanic123@gmail.com>
2022-12-14 16:55:20 +0100

    Add test in admin.cy.ts for add member in team

--- packages/SiteVGS-e2e/src/e2e/admin.cy.ts
+++ packages/SiteVGS-e2e/src/e2e/admin.cy.ts
@@ -14,9 +14,7 @@ describe('Admin', () => {
 
     // Check si on est bien renvoyé à l'accueil à la suite de la connexion
     cy.url().should('include', '/index.php')
-  })
 
-  it('Delete Member', () => {
     // Check si dans la navbar le lien paramètre est bien là
     cy.get('[data-cy="nav-ul"]').children().should('contain', 'PARAMÈTRE')
     cy.get('[data-cy="settings"]').click()
@@ -26,20 +24,50 @@ describe('Admin', () => {
     cy.get('[data-cy="nav-ul-admin"]').children().should('contain', 'Config')
     cy.get('[data-cy="config"]').click()
     cy.url().should('include', '/admin/config.php')
+  })
 
-    // 
-    cy.get('[data-cy="delete-members"]').click()
+  it('Add Member on Team', () => {
+    cy.get('[data-cy="add-member-team"]').should('exist').click()
+    cy.url().should('include', '/admin/add_member.php')
+    cy.get('[data-cy="add-member-img"]').should('exist').selectFile('/packages/SiteVGS/img/team/test.jpg')
+    cy.get('[data-cy="add-member-username"]').should('exist').type('userTest')
+    cy.get('[data-cy="add-member-role"]').should('exist').type('clean')
+    cy.get('[data-cy="add-member-grade"]').should('exist').type('sous-chef')
+    cy.get('[data-cy="add-member-submit"]').should('exist').click()
+    cy.url().should('include', '/admin/config.php')
+    cy.get('[data-cy="update-member-team"]').should('exist').click()
+    cy.url().should('include', '/admin/select_team.php')
+    cy.get('[data-cy="update-member-team-username"]').should('exist')
+  })
+
+  it('Update Member on Team', () => {
+    cy.get('[data-cy="update-member-team"]').should('exist').click()
+  })
+
+  it('Delete Member of Team', () => {
+    // Delete Member of team and check it
+    cy.get('[data-cy="delete-members-team"]').should('exist').click()
+    cy.url().should('include', '/admin/delete_member_team.php')
+    cy.get('[data-cy="delete-member-team"]').check('userTest')
+    cy.get('[data-cy="delete-member-team-submit"]').should('exist').click()
+    cy.get('[data-cy="delete-members-team"]').click()
+    cy.get('[data-cy="pseudo-member-team"]').contains('userTest').should('not.exist')
+  })
+
+  it('Delete Member', () => {
+    // Delete Member and check it
+    cy.get('[data-cy="delete-members"]').should('exist').click()
     cy.url().should('include', '/admin/delete_member.php')
-    cy.get('[data-cy="delete-member"]').check('Floflo1')
-    cy.get('[data-cy="delete-member-submit"]').click()
+    cy.get('[data-cy="delete-member"]').check('userTest')
+    cy.get('[data-cy="delete-member-submit"]').should('exist').click()
     cy.get('[data-cy="delete-members"]').click()
-    cy.get('[data-cy="pseudo-member"]').contains('Floflo1').should('not.exist')
+    cy.get('[data-cy="pseudo-member"]').contains('userTest').should('not.exist')
   })
 
   afterEach(() => {
     // logout
-    cy.get('[data-cy="nav-ul"]').children().should('contain', 'DÉCONNEXION')
-    cy.get('[data-cy="login"]').click()
+    cy.get('[data-cy="nav-ul-admin"]').children().should('contain', 'Deconnexion')
+    cy.get('[data-cy="logout"]').click()
     cy.url().should('include', '/index.php')
   })
 });
